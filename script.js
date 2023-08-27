26b1a54823f67e39ea23234d44230b37ecf8a524
Hlamx <88639120+hlamx@users.noreply.github.com>
2023-08-27 13:32:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('animationX');
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('animationX');
         }
     });
 });
 
 const allElements = document.querySelectorAll('.loading');
 allElements.forEach((el) => observer.observe(el));
+// ------------------------------- //
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animationY');
+        } else {
+            entry.target.classList.remove('animationY');
+        }
+    });
+});
+
+const allElements1 = document.querySelectorAll('.loading1');
+allElements1.forEach((el) => observer1.observe(el));
